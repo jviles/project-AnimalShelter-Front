c5c3a35eaf693f9edb76e8f2d8bbb0f3cfc6a57a
jviles <jvilesheras@hotmail.com>
2017-10-10 15:23:38 +0200

    materilize working

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { any } from 'codelyzer/util/function';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+declare var $ ;
+declare var jQuery;
+
+
+$(document).ready(function(){
+$('.slider').slider({
+  indicators:false
+});
+});
 
 
 
